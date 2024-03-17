906d7cc0a0a13237122562e1adfbfeb5f7463109
Andrej Nenadić <andrejnana445@gmail.com>
2024-03-17 23:19:42 +0100

    Replaced the simple cube model with a simple monkey head model, proof of concept

--- src/Menu/Cube/Cube.tsx
+++ src/Menu/Cube/Cube.tsx
@@ -1,85 +1,55 @@
 import { useEffect, useRef } from 'react';
 import { Mesh } from 'three';
-import { useFrame, useThree } from '@react-three/fiber';
+import { useFrame, useLoader, useThree } from '@react-three/fiber';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 type CubeProps = {
     selectedItemId: number;
     onClick: () => void;
 }
 
-/**
- * Component representing a spinning cube.
- * 
- * @param {CubeProps} props - The props for the cube.
- * @param {number} props.selectedItemId - The index of the currently selected item.
- * @param {() => void} props.onClick - The callback function when the cube is clicked.
- * @returns {JSX.Element} The rendered cube.
- */
 export default function Cube({ selectedItemId, onClick }: CubeProps): JSX.Element {
-    // Reference to the cube mesh.
+    const gltf = useLoader(GLTFLoader, '/3DModels/monkey.glb');
+
     const cubeRef = useRef<Mesh>(null);
-    // Reference to the previous selected item ID.
     const previousSelectedItemId = useRef(selectedItemId);
-    // Reference to the spin direction. Positive value spins clockwise, negative value spins counter-clockwise.
     const spinDirection = useRef(1);
 
-    // Hook for GSAP.
     const { contextSafe } = useGSAP();
 
-    /**
-     * Spins the cube by the specified number of full rotations.
-     * 
-     * @param {number} n - The number of full rotations to spin. Positive values spin clockwise, negative values spin counter-clockwise.
-     */
     const spin = contextSafe((n: number) => {
-        // The spin function is responsible for rotating the cube's Y-axis by the specified number of full rotations.
-        // This is done using GSAP for smooth animations.
-
-        // If the cubeRef is not null, meaning the cube has been rendered, we animate its rotation using gsap.to.
-        // The target of the animation is the cube's rotation, and we animate the Y-axis rotation by adding the specified number of full rotations.
-        // The duration of the animation is 1 + n * 0.1 second, and the ease is set to 'sin.inOut' for a smooth sine wave motion.
         if (cubeRef.current) {
             gsap.to(cubeRef.current.rotation, {
-                y: `${spinDirection.current < 0 ? '-=' : '+='}${3.14 * (n > 7 ? n / 2 : n)}`, // Add the specified number of full rotations to the current rotation
-                duration: 1 + n * 0.1, // Duration of 1 + n * 0.1 second
-                ease: 'sin.inOut', // Smooth sine wave motion
+                y: `${spinDirection.current < 0 ? '-=' : '+='}${3.14 * (n > 7 ? n / 2 : n)}`,
+                duration: 1 + n * 0.1,
+                ease: 'sin.inOut',
             });
         }
     });
 
-    // Effect hook to handle spin animation when the selected item ID changes.
     useEffect(() => {
         if (previousSelectedItemId.current !== selectedItemId) {
-            // Calculate the difference between the previous and current selected item IDs.
             let difference = Math.abs(previousSelectedItemId.current - selectedItemId);
-            // Limit the difference to a minimum of 1 and a maximum of 30.
             difference = Math.min(difference, 30 - difference);
-            // Set the spin direction based on the difference.
             spinDirection.current = Math.sign(previousSelectedItemId.current - selectedItemId);
 
-            // Spin the cube by the calculated difference.
             spin(difference);
-            // Update the previous selected item ID.
             previousSelectedItemId.current = selectedItemId;
         }
     }, [selectedItemId, spin]);
 
-    // useFrame hook to continuously rotate the cube.
     useFrame(() => {
         if (cubeRef.current) {
-            cubeRef.current.rotation.x += 0.001 * spinDirection.current;
+            cubeRef.current.rotation.x += 0.001;
             cubeRef.current.rotation.y += 0.005 * spinDirection.current;
         }
     });
 
-    // Hook to get the viewport size.
     const viewport = useThree(state => state.viewport);
-    // Calculate the scale of the cube based on the viewport size.
     const scale = Math.min(viewport.width, viewport.height) / 2.7;
 
-    // Render the cube with a spot light and a cube mesh.
     return (
         <>
             <spotLight
@@ -91,8 +61,12 @@ export default function Cube({ selectedItemId, onClick }: CubeProps): JSX.Elemen
                 color={'#ffd'}
                 target={cubeRef.current ?? undefined} />
 
-            <mesh ref={cubeRef} receiveShadow castShadow onClick={onClick} scale={scale}>
-                <boxGeometry />
+            <mesh ref={cubeRef} receiveShadow castShadow onClick={onClick} geometry={
+                //@ts-expect-error
+                gltf.scene.children[0].geometry
+            } scale={scale / 1.5}>
+
+                {/* <boxGeometry /> */}
                 <meshStandardMaterial color={'#fff'} />
             </mesh>
         </>
